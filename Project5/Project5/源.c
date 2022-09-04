9ceeace58f932674e291194a00db80d3e4dcf0c1
Vellichor-Chen <112534356+Vellichor-Chen@users.noreply.github.com>
2022-09-04 10:56:29 +0800

    Add files via upload

---
+++ Project5/Project5/源.c
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (5 == i)
+			continue;
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+ 
+	/*int day = 0;
+	int a = 1;
+	//scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+		if(a =
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("工作日\n");
+		break;
+	case 6:
+	case 7:
+		printf("休息日\n");
+		break;
+	default:
+		printf("输入错误\n");
+		break;*/
+	/*case 1:
+		printf("星期1\n");
+		break;
+	case 2:
+		printf("星期2\n");
+		break;
+	case 3:
+		printf("星期3\n");
+		break;
+	case 4:
+		printf("星期4\n");
+		break;
+	case 5:
+		printf("星期5\n");
+		break;
+	case 6:
+		printf("星期6\n");
+		break;
+	case 7:
+		printf("星期7\n");
+		break;*/
+	
+
+	/*int i = 1;
+	while (i <= 100)
+	{
+		if (i % 2 == 1)
+			printf("%d ", i);
+		i++;
+	}*/
+
+
+	//int a = 0;
+	//int b = 2;
+	//if (a == 1)
+	//{
+	//	if (b == 2)
+	//		printf("hehe\n");
+	//}
+	//else
+	//		printf("haha\n");
+
+	/*int age = 10;
+	if (age < 18)
+	{
+		printf("未成年\n");
+		printf("不能谈恋爱\n");
+	}
+	else
+	{
+		if (age >= 18 && age < 28)
+			printf("青年\n");
+		else if (age >= 28 && age < 50)
+			printf("壮年\n");
+		else if (age >= 50 && age < 90)
+			printf("老年\n");
+		else
+			printf("老不死\n");
+	}*/
+	//return 0;
+//}//
