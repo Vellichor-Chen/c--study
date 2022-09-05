2a473b63c20568d1d007d5f5ea6fee9a3a160837
Vellichor-Chen <112534356+Vellichor-Chen@users.noreply.github.com>
2022-09-05 10:16:51 +0800

    Add files via upload

---
+++ Project6/Project6/源.c
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{	
+
+
+
+	/*int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到\n");
+	}*/
+
+	/*for (i = 0;i < sz;i++)
+	{
+		if (k == arr[i])
+		{
+			printf("找到了，下标是：%d\n", i);
+			break;
+		}
+	}
+	if (i == sz)
+	{
+		printf("找不到\n");
+	}*/
+
+
+
+	//int i = 0;
+	//int n = 0; 
+	//int sum = 0;
+	//int ret = 1;
+	//for (n = 1;n <= 3;n++)
+	//{ //scanf("%d", &n);
+	//	ret = ret * n;
+	//	sum = sum + ret;
+	//}
+	//	printf("sum = %d\n", sum);
+	//
+
+		/*int i = 0;
+	for (i = 1;i <= 10;i++)
+	{
+	
+	}*/
+
+
+	//int ret = 0;
+	//char ch = 0;
+	//char password[20] = { 0 };
+	//printf("请输入密码:>\n");
+	//scanf("%s", password);
+	//printf("请确认(Y/N):>\n");
+	//while((ch=getchar())!='\n')
+	//getchar();//读取\n
+	//ret = getchar();
+	//if (ret == 'Y')
+	//{
+	//	printf("确认成功\n");
+	//}
+	//else
+	//{
+	//	printf("确认失败\n");
+	//}
+
+
+	/*int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}*/
+	return 0;
+}
