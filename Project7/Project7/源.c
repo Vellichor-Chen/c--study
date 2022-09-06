03e1d9a3e9e2984edb6d01445e65fb0dae46d05a
Vellichor-Chen <112534356+Vellichor-Chen@users.noreply.github.com>
2022-09-06 13:12:22 +0800

    Add files via upload

---
+++ Project7/Project7/源.c
@@ -0,0 +1,194 @@
+
+
+
+
+
+
+
+
+
+
+
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//
+//
+//
+//	/*int i = 0;
+//	for (i = 0;i <= 9;i++)
+//	{
+//		int j = 1;
+//		for (j = 1;j <= i;j++)
+//		{
+//			printf(" %d * %d = %-2d  ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}*/
+//
+//
+//	/*int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 1;i < sz; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("max = %d\n", max);*/
+//
+//	/*int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//	for (i = 1;i <= 100;i++)
+//	{
+//		sum += flag* 1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);*/
+//
+//	/*int i = 0;
+//	int count = 0;
+//	for (i = 1;i <= 100;i++)
+//	{
+//		if (i % 10 == 9)
+//		{
+//			count++;
+//		}
+//		if (i / 10 == 9)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("count=%d\n", count);*/
+//
+//	/*int i = 0;
+//	int count = 0;
+//	for (i = 101;i <= 200;i+=2)
+//	{
+//		int j = 0;
+//		for (j = 2;j <= sqrt(i);j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j > sqrt(i))
+//		{
+//			count++;
+//			printf("%d ", i);
+//			printf("\ncount=%d\n", count);
+//		}*/
+//
+//	//}
+//
+//	/*int i = 0;
+//	int count = 0;
+//	for (i = 100;i <= 200;i++)
+//	{
+//		int j = 0;
+//		for (j = 2;j <= sqrt(i);j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j > sqrt(i))
+//		{
+//			count++;
+//			printf("%d ", i);
+//			printf("\ncount=%d\n", count);
+//		}
+//
+//	}*/
+//
+//	/*int i = 0;
+//	int count = 0;
+//	for (i = 100;i <= 200;i++)
+//	{
+//		int j = 0;
+//		for (j = 2;j < i;j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j == i)
+//		{
+//			count++;
+//			printf("%d ", i);
+//			printf("\ncount=%d\n", count);
+//		}
+//		
+//	}*/
+//
+//	/*int year = 0;
+//	int count = 0;
+//	for (year = 1000;year <= 2000; year++)
+//	{
+//		if (year % 4 == 0  && year % 100 != 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//		else if (year % 400 == 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//	}
+//	printf("\ncount = %d\n", count);*/
+//
+//	/*int a = 0;
+//	int b = 0;
+//	int r = 0;
+//	scanf("%d%d", &a, &b);
+//	while (a % b)
+//	{
+//		r = a % b;
+//		a = b;
+//		b = r;
+//	}
+//	printf("%d\n", r);*/
+//
+//	/*int i = 0;
+//	for (i = 1;i <= 100;i++)
+//	{
+//		if (i % 3 == 0)
+//		{
+//			printf("%d ", i);
+//		}*/
+//	//}
+//
+//	/*int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	 }
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d %d %d\n", a, b, c);*/
+//	return 0;
+//}
