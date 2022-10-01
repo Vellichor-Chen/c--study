dcdef684d7c626d76314883b0bd55070e9749cba
Vellichor-Chen <112534356+Vellichor-Chen@users.noreply.github.com>
2022-10-02 01:36:12 +0800

    Add files via upload

---
+++ test_1/test_1/源.cpp
@@ -0,0 +1,60 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+    printf("Practice makes perfect!");
+    return 0;
+}
+#include <stdio.h>
+int main()
+{
+    printf("v   v\n v v\n  v\n");
+    return 0;
+}
+#include<stdio.h>
+int main()
+{
+    printf("The size of short is %d bytes.\n", sizeof(short));
+    printf("The size of int is %d bytes.\n", sizeof(int));
+    printf("The size of long is %d bytes.\n", sizeof(long));
+    printf("The size of long long is %d bytes.\n", sizeof(long long));
+    return 0;
+}
+#include<stdio.h>
+int main()
+{
+    printf("     **     \n");
+    printf("     **     \n");
+    printf("************\n");
+    printf("************\n");
+    printf("    *  *    \n");
+    printf("    *  *    \n");
+
+
+
+    return 0;
+#include<stdio.h>
+    int main()
+    {
+        printf("0%o 0X%X", 1234, 1234);
+        return 0;
+    }
+
+
+#include<stdio.h>
+    int main()
+    {
+        printf("%15d", 0xABCDEF);
+        return 0;
+    }
+
+#include<stdio.h>
+    int main()
+    {
+        int i = printf("Hello world!");
+        printf("\n");
+        printf("%d", i);
+        return 0;
+    }
+}
