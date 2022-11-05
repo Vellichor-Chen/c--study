f7a9266cb8e5b3847409223c4ed80a8ce197d93f
Vellichor-Chen <112534356+Vellichor-Chen@users.noreply.github.com>
2022-11-05 08:21:24 +0800

    Add files via upload

---
+++ 冒泡排序/冒泡排序/源.cpp
@@ -0,0 +1,51 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0;i < sz - 1;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < sz - 1 - i;j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	int i = 0;
+//	for (i = 0;i < sz;i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+int cmp_int(const void*e1,const void*e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+int main()
+{
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	qsort(arr, sz, sizeof(arr[0]),cmp_int );
+	for (i = 0;i < sz;i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
