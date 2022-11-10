2dd84c67a885d5e053f9e3384be905d02de9272c
Vellichor-Chen <112534356+Vellichor-Chen@users.noreply.github.com>
2022-11-11 00:34:29 +0800

    Add files via upload

---
+++ 通讯录-改1/通讯录-改/test.c
@@ -0,0 +1,56 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include"contact.h"
+void menu()
+{
+	printf("*******************************************\n");
+	printf("****     1.add               2.del     ****\n");
+	printf("****     3.search            4.modify  ****\n");
+	printf("****     5.show             6.sort    ****\n");
+	printf("****                0.exit             ****\n");
+	printf("*******************************************\n");
+
+}
+int main()
+{
+	int input = 0;
+	//创建通讯录
+	struct contact con;//通讯录，包含1000的元素和size
+	//初始化
+	initcontact(&con);
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			addcontact(&con);
+			break;
+		case DEL:
+			delcontact(&con);
+			break;
+		case SEARCH:
+			searchcontact(&con);
+			break;
+		case MODIFY:
+			modifycontact(&con);
+			break;
+		case SHOW:
+			showcontact(&con);
+			break;
+		case SORT:
+			sortcontact(&con);
+			break;
+		case EXIT:
+			//销毁通讯录，释放动态开辟的内存
+			destroycontact();
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
